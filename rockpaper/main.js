5e66724ac2339531f0d2a344e61e402f4eb4f1ee
verma-riya20 <157815520+verma-riya20@users.noreply.github.com>
2024-02-01 01:43:25 +0530

    Add files via upload

---
+++ rockpaper/main.js
@@ -0,0 +1,47 @@
+const playerChoice=document.querySelector("#playerChoice");
+const computerChoice=document.querySelector("#computerChoice");
+const resulttext=document.querySelector("#resulttext");
+const choicebtn=document.querySelectorAll(".choicebtn");
+let player;
+let computer;
+let result;
+choicebtn.forEach(button=> button.addEventListener("click",()=>{
+    player=button.textContent;
+    Computerturn();
+    playerChoice.textContent=`Player: ${player}`;
+    computerChoice.textContent=`Computer: ${computer}`;
+    resulttext.textContent=checkwinner()
+
+}))
+const Computerturn=()=>{
+    const randomNo=Math.floor(Math.random()*3);
+    switch(randomNo){
+        case 1:
+            computer="ROCK";
+            break;
+        case 2:
+                computer="PAPER";
+                break;
+    
+        case 3:
+                    computer="SCISSORS";
+                    break;
+        
+        }
+   
+}
+function checkwinner(){
+    if (player==computer){
+        return "Draw!";
+    }
+    else if(computer=="ROCK"){
+        return (player=="PAPER")?"You Win!":" You Lose!";
+    }
+    else if(computer=="PAPER"){
+        return (player=="SCISSORS")?"You Win!": "You Lose!";
+    }
+    else if(computer=="SCISSORS"){
+        return (player=="ROCK")?"You Win!": "You Lose!";
+    }
+    
+}
